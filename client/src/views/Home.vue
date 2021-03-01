7bda73117e9fc3a575f8825b719eb619f1dbe6e5
Benbinbin <3126319718@qq.com>
2021-03-01 23:35:16 +0800

    add tailwind and home page view.

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,27 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home grid grid-cols-10 h-screen">
+    <aside class="col-span-2 border-2 border-yellow-400 flex flex-col">
+      <div class="profile h-24 lg:h-32 border-2 border-blue-400"></div>
+      <div class="category flex-grow border-2 border-blue-400"></div>
+      <div class="add h-14 border-2 border-blue-400"></div>
+    </aside>
+    <div class="col-span-8 border-2 border-yellow-400">
+      <nav class="profile h-24 lg:h-32 border-2 border-indigo-400">书库</nav>
+      <main>
+        <section>
+          <h3>在读</h3>
+        </section>
+      </main>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
   },
 };
 </script>
