5e7461fb3f88eb9634af52ba9b151985490716ee
Benbinbin <3126319718@qq.com>
2021-05-15 22:01:29 +0800

    optimize tailwind build.

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,11 @@
 module.exports = {
-  purge: [],
+  // purge: [],
   // purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  purge: [
+    './src/**/*.html',
+    './src/**/*.vue',
+    './src/**/*.jsx',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
